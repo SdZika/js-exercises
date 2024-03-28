5d80a18c3ba28bc246a66f32dc0656fce0172cbb
SdZika <markozivkovic1982@gmail.com>
2024-03-28 19:13:53 +0100

    done object transformation

--- object-transformation.js
+++ object-transformation.js
@@ -1,12 +1,16 @@
 let  studentGrade = [{marko: "A"}, {biljana: "B"}, {dunja: "C"}, {vukan: "D"}];
 let newObject = {};
 
+
+
 for (let i = 0; i < studentGrade.length; i++) {
     
-   for (let key in newObject[i]) {
+   for (let key in studentGrade[i]) {
      
-    newObject.key[i];
-     console.log(newObject);
+      newObject[key] = studentGrade[i][key];
+   
+   
    }
    
 }
+console.log(newObject);
