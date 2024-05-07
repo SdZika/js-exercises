fa052eb31936afd454f2ee941d878c597698caab
SdZika <markozivkovic1982@gmail.com>
2024-05-07 14:38:38 +0200

    done triangle

---
+++ triangle-of-numbers.js
@@ -0,0 +1,27 @@
+
+/*let num = 5;
+for (let i = 1; i <= num; i ++){
+    let row = "";
+    for (let j = 1; j <= i; j ++){
+        
+        row = row + j + " ";
+        
+    }
+    console.log(row);
+    
+}*/
+
+function triangle (num){
+    for (let i = 1; i <= num; i ++){
+        let row = "";
+        for (let j = 1; j <= i; j ++){
+            
+            row = row + j + " ";
+            
+        }
+        console.log(row);
+        
+    }
+}
+
+triangle(5);
